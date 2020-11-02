f86ce784d8d61d2f87b45f69e085f705c67a9984
unknown <petter.fogel@medieinstitutet.se>
2020-11-02 18:24:53 +0100

    Removed content to other folders.

--- script.js
+++ script.js
@@ -30,23 +30,6 @@ sideBar.onmouseout = function() {
 // }
 
 
-/* === IMAGE SLIDE-SHOW === */
-setInterval(startSlideShow, 3000)
-const imageOne = document.getElementById("image1");
-const imageTwo = document.getElementById("image2");
-const imageThree = document.getElementById("image3");
-const imageArray = [imageOne, imageTwo, imageThree];
 
-function startSlideShow(){
-    for(i = 0; i < imageArray.length; i++){
-        if(imageArray[i].classList.contains("show")){
-            imageArray[i].classList.toggle("show")
-            imageArray[2].classList.toggle("show")
-        } else if (imageArray[2].classList.contains("show")){
-            imageArray[i].classList.toggle("show")
-            imageArray[2].classList.toggle("show")
-        }
-    }
-}
 
 /* === JUMPING MAN === */
