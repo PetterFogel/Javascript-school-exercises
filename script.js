83fb08a4014c76fa7ba2ee42b523cfc0e25fa13a
unknown <petter.fogel@medieinstitutet.se>
2020-11-02 19:40:14 +0100

    Added burger icon, layout and toggle logic.

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const sideBar = document.querySelector(".sidebar");
 const userProfile = document.querySelector(".user-profile");
 const linkTitles = document.querySelector(".link-title");
 const settingsLink = document.querySelector(".settings");
+const burgerIcon = document.querySelector(".burger");
 
 sideBar.onmouseover = function() {
     if(window.innerWidth > 1200){
@@ -14,22 +15,9 @@ sideBar.onmouseout = function() {
     sideBar.style.width = "4rem";
     } 
 }
-// window.onresize = function() {
-//     if(window.innerWidth > 1200){
-//         sideBar.style.width = "4rem"; 
-//         sideBar.style.marginLeft = "0";
-//         userProfile.style.display = "none";
-//         linkTitles.style.display = "none";
-//     }
-//     else if (window.innerWidth < 1200) { 
-//         sideBar.style.width = "15rem";
-//         sideBar.style.marginLeft = "0";
-//         userProfile.style.display = "block";
-//         linkTitles.style.display = "block";
-//     }
-// }
 
-
-
-
-/* === JUMPING MAN === */
+burgerIcon.onclick = function() {
+    sideBar.classList.toggle("sidebar-active");
+    burgerIcon.classList.toggle("burger-active");
+    settingsLink.style.display = "block";
+} 
